22c9a851e872a2e7538365065812fdf312258dea
git4ruby <chandanamohit@gmail.com>
2016-05-26 23:38:58 -0500

    Added Person credibility

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,53 @@
+class PeopleController < ApplicationController
+  before_action :find_person, only: [:show, :edit, :update, :destroy]
+
+  def index
+
+  end
+
+  def create
+    @person = Person.new person_params
+
+    if @person.save
+      redirect_to @person, notice: "Heya Omkar! Person details have succesfully saved!!!"
+    else
+      render 'new', notice: "Oops! Person details couldn't be saved!!!"
+    end
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @person.update person_params
+      redirect_to @person, notice: "Hurray! Person details updated!!!"
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    @person.destroy
+    redirect_to people_path
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :age, :nationality, :contact, :height, :gender, :description, :missing_date, :missing_location, :updates)
+  end
+
+  def find_person
+    @person = Person.find(params[:id])
+  end
+
+end
