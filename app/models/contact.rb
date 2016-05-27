f9f95da49807a5ec67eee6a2331d412e883a925a
git4ruby <chandanamohit@gmail.com>
2016-05-27 18:10:48 -0500

    Added phone number to contact form and multipart to contact#new page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
+  attribute :phone, presence: true
   attribute :images
   attribute :nickname,  :captcha  => true
 
@@ -10,6 +11,7 @@ class Contact < MailForm::Base
       :subject => "Contact Form for Missing Person",
       :to => "email.swar@gmail.com",
       :from => %("#{name}" <#{email}>)
+
     }
   end
 end
