5acc532c309cbfda028ede8b0c4f9c7dc80b83b7
git4ruby <chandanamohit@gmail.com>
2016-05-27 16:46:13 -0500

    Added validations to person details

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,11 @@ class Person < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "400x400#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, numericality: { only_integer: true }
+  validates :nationality, presence: true
+  validates :gender, presence: true
+  validates :missing_date, presence: true
+
 end
